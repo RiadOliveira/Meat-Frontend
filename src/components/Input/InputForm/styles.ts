730372238cbf82f8def366abd5a89b33bc2b8fa2
DaviEmanuelll <daviemanuelll04@gmail.com>
2022-11-01 23:49:00 -0300

    InputForm Components

---
+++ src/components/Input/InputForm/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { palette } from 'assets/colors/palette';
+
+export const InputForm = styled.div`
+  position: relative;
+  input {
+    outline: none;
+    border: 0;
+    border-bottom: solid 2.5px ${palette.blueHigh};
+    background: transparent;
+    padding: 1rem;
+    font-size: 1rem;
+    color: ${palette.blue};
+    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+  label {
+    position: absolute;
+    left: 16px;
+    color: ${palette.blue};
+    pointer-events: none;
+    transform: translateY(1rem);
+    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+  input:focus {
+    outline: none;
+    border-bottom: solid 2.5px ${palette.pinkHigh};
+  }
+  input:focus ~ label {
+    transform: translateY(-50%) scale(0.8);
+    background-color: ${palette.white};
+    padding: 0 0.2em;
+    color: ${palette.blueHigh};
+  }
+  select {
+    outline: none;
+    border: 0;
+    border-bottom: solid 2.5px ${palette.blueHigh};
+    background: transparent;
+    padding: 1rem;
+    font-size: 1rem;
+    color: ${palette.blue};
+    transition: 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+`;
