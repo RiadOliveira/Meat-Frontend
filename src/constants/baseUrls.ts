3427187462e8b22eead57a44e6be077c0fc9607a
RiadOliveira <riad.oliveira@hotmail.com>
2022-11-08 07:56:50 -0300

    URLs fixed

--- src/constants/baseUrls.ts
+++ src/constants/baseUrls.ts
@@ -1,2 +1,2 @@
-export const frontendUrl = 'http://10.215.4.20:3000';
-export const backendUrl = 'http://10.215.4.20:3333';
+export const frontendUrl = 'http://localhost:3000';
+export const backendUrl = 'http://localhost:3333';
